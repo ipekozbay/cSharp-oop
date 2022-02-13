cb112edd5a9c490d8874d21a5120fb019ad00074
ipekozbay <20060401@stu.omu.edu.tr>
2022-02-13 14:49:29 +0300

    folder fix

---
+++ OOP/IndividualCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class IndividualCustomer : Customer
+    {
+        public string NationalityId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
